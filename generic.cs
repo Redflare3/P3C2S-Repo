2689e1b61b18477093625fb2c4cea81d2346da08
Sund64 <chris.cornelis.lukito@gmail.com>
2026-05-08 13:24:52 +0700

    penambahan fitur automata

--- generic.cs
+++ generic.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Express is running!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+using System;
 using System.Collections.Generic;
 
 class repository<T>
 {
     private List<T> entity = new List<T>();
 
-    public void add(T item)
-    {
-        entity.Add(item);
-    }
+    public void add(T item) => entity.Add(item);
 
     public List<T> GetAll() => entity;
 
@@ -17,57 +25,112 @@ class repository<T>
         List<T> hasilfilter = new List<T>();
         foreach (var item in entity)
         {
-            if (item is rumah rumah)
+            if (item is rumah r && r.status == statusKetersediaan.Tersedia)
             {
-                if (rumah.statusKetersediaan == true)
-                {
-                    hasilfilter.Add(item);
-                }
+                hasilfilter.Add(item);
             }
         }
-
         return hasilfilter;
     }
-}
+    public List<T> filterDibooking()
+    {
+        List<T> hasilFilter = new List<T>();
+        foreach (var item in entity)
+        {
+            if (item is rumah r && r.status == statusKetersediaan.Dibooking)
+            {
+                hasilFilter.Add(item);
+            }
+        }
+        return hasilFilter;
+    }
+    public List<T> filterLunas()
+    {
+        List<T> hasilFilter = new List<T>();
+        foreach (var item in entity)
+        {
+            if (item is rumah r && r.status == statusKetersediaan.Terjual)
+            {
+                hasilFilter.Add(item);
+            }
+        }
+        return hasilFilter;
+    }
 
+}
+public enum statusKetersediaan
+{
+    Tersedia,
+    Dibooking,
+    Terjual
+}
+public enum statusPembayaran
+{
+    PembatalanPembayaran,
+    ProsesCicilan,
+    Lunas
+}
 public class rumah
 {
     public int idRumah { get; set; }
     public string blokNomor { get; set; }
     public double harga { get; set; }
-    public bool statusKetersediaan { get; set; }
-}
-
-class program
-{
-    static void Main(string[] args)
+    public statusKetersediaan status { get; set; } = statusKetersediaan.Tersedia;
+    public void updateStatus(statusPembayaran trigger)
     {
-        var repoTapalosa = new repository<rumah>();
-        repoTapalosa.add(new rumah
+        switch (status)
         {
-            idRumah = 1,
-            blokNomor = "A1",
-            harga = 172000050,
-            statusKetersediaan = true
-        });
-        repoTapalosa.add(new rumah
-        {
-            idRumah = 2,
-            blokNomor = "A2",
-            harga = 172000050,
-            statusKetersediaan = false
-        });
-
-        Console.WriteLine("informasi rumah: ");
-        foreach (var item in repoTapalosa.GetAll())
-        {
-            Console.WriteLine($"Rumah {item.idRumah}, Harga: {item.harga}, Tersedia: {item.statusKetersediaan}");
+            case statusKetersediaan.Tersedia:
+                if (trigger == statusPembayaran.ProsesCicilan)
+                    status = statusKetersediaan.Dibooking;
+                break;
+            case statusKetersediaan.Dibooking:
+                if (trigger == statusPembayaran.PembatalanPembayaran)
+                    status = statusKetersediaan.Tersedia;
+                else if (trigger == statusPembayaran.Lunas)
+                    status = statusKetersediaan.Terjual;
+                break;
+            case statusKetersediaan.Terjual:
+                Console.WriteLine($"Rumah {idRumah} sudah terjual, status tidak bisa diubah lagi.");
+                break;
         }
+    }
 
-        Console.WriteLine("rumah tersedia: ");
-        foreach (var item in repoTapalosa.filterTersedia())
+    class program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine($"Rumah {item.idRumah}, Harga: {item.harga}");
+            var repoTapalosa = new repository<rumah>();
+            var rumah1 = new rumah { idRumah = 1, blokNomor = "A1", harga = 172000050 };
+            var rumah2 = new rumah { idRumah = 2, blokNomor = "A2", harga = 172000050 };
+            repoTapalosa.add(rumah1);
+            repoTapalosa.add(rumah2);
+            // test logic automata 
+            rumah1.updateStatus(statusPembayaran.ProsesCicilan);
+            Console.WriteLine($"Status rumah 1 sekarang: {rumah1.status}");
+            rumah1.updateStatus(statusPembayaran.Lunas);
+            Console.WriteLine($"Status rumah 1 sekarang: {rumah1.status}");
+            Console.WriteLine("informasi rumah: ");
+            foreach (var item in repoTapalosa.GetAll())
+            {
+                Console.WriteLine($"Rumah {item.idRumah}, Harga: {item.harga}, Status rumah: {item.status}");
+            }
+            // list status rumah
+            Console.WriteLine("rumah tersedia: ");
+            foreach (var item in repoTapalosa.filterTersedia())
+            {
+                Console.WriteLine($"Rumah {item.idRumah}, Harga: {item.harga}");
+            }
+            Console.WriteLine("rumah dibooking: ");
+            foreach (var item in repoTapalosa.filterDibooking())
+            {
+                Console.WriteLine($"Rumah {item.idRumah}, Blok Nomor {item.blokNomor}");
+            }
+            Console.WriteLine("rumah lunas: ");
+            foreach (var item in repoTapalosa.filterLunas())
+            {
+                Console.WriteLine($"Rumah {item.idRumah}, Blok Nomor {item.blokNomor}");
+            }
         }
     }
 }
