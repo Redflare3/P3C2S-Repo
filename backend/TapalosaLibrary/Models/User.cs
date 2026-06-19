6a1dda02f955c331695ca8fc035acc06665929ed
Sund64 <chris.cornelis.lukito@gmail.com>
2026-06-19 11:46:49 +0700

    Add files via upload

---
+++ backend/TapalosaLibrary/Models/User.cs
@@ -0,0 +1,12 @@
+namespace TapalosaCommon.Models
+{
+    public class User
+    {
+        public int idUser { get; set; }
+        public string nama { get; set; } = string.Empty;
+        public string role { get; set; } = string.Empty;
+        public string status { get; set; } = string.Empty;
+        public string username { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
